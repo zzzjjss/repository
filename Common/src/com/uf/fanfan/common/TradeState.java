b69123685e23171d7d2a10feac3db8f81385fb6b
zzzjjss <zzzjjss@gmail.com>
2013-08-02 17:01:44 +0800

    add tradeState

---
+++ Common/src/com/uf/fanfan/common/TradeState.java
@@ -0,0 +1,9 @@
+package com.uf.fanfan.common;
+
+public class TradeState {
+	public static final short  PROCESSING=0;
+	public static final short  SURED=1;
+	public static final short  SUCESS=2;
+	public static final short  FAIL=3;
+	
+}
