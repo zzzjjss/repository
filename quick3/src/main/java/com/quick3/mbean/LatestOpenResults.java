4e9515b4a3f1e57db4aa26407902caa9cc1c5b18
jason <jason@jason-PC>
2014-12-10 20:10:10 +0800

    fix the bug

--- quick3/src/main/java/com/quick3/mbean/LatestOpenResults.java
+++ quick3/src/main/java/com/quick3/mbean/LatestOpenResults.java
@@ -2,15 +2,12 @@ package com.quick3.mbean;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.management.AttributeChangeNotification;
 import javax.management.NotificationBroadcasterSupport;
 
 import com.quick3.OpenResult;
-import com.quick3.StatisticsTool;
-import com.quick3.bean.StatisticResult;
 
 public class LatestOpenResults extends NotificationBroadcasterSupport implements LatestOpenResultsMBean{
 	private List<OpenResult> latestOpenResults=new ArrayList<OpenResult>();
