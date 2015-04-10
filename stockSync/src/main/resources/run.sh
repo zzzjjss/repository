28dd7d90983cc78391949e121f3e478a0b31c160
ZhangJa <ZhangJa@ZHANGJA-7W1.analytics.moodys.net>
2015-04-10 16:16:35 +0800

    unix format

--- stockSync/src/main/resources/run.sh
+++ stockSync/src/main/resources/run.sh
@@ -1,13 +1,13 @@
 if [ -z $JAVA_HOME ]; then
-        echo "the JAVA_HOME is  empty."
-        exit 1;
+	echo "the JAVA_HOME is  empty."
+	exit 1;
 fi
 scriptPath=`readlink  -f  "$0"`
 scriptFolder=`dirname "$scriptPath"`
 
 classPath=""
-for  f in "$scriptFolder"/lib/*.jar; do
-        classPath=${classPath}${f}":"
+for  f in "$scriptFolder"/lib/*.jar; do 
+	classPath=${classPath}${f}":"
 done
-#-agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=y
-${JAVA_HOME}/bin/java  -DbasePath=$scriptFolder  -classpath $classPath   com.uf.stock.Main
+#-agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=y 
+${JAVA_HOME}/bin/java  -DbasePath=$scriptFolder  -classpath $classPath   com.uf.stock.Main  
