dfa767ca0e8b811ff92faff3fc1c503be2dfef0c
adm_ZhangJa <adm_ZhangJa@ZHANGJA-7W1.analytics.moodys.net>
2016-12-02 18:30:49 +0800

    add sniffer

--- PowerSniffer/src/main/java/com/uf/stock/restful/RestfulLauncher.java
+++ PowerSniffer/src/main/java/com/uf/stock/restful/RestfulLauncher.java
@@ -4,9 +4,14 @@ import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
+import com.uf.stock.sniffer.Monitor;
+import com.uf.stock.util.SpringBeanFactory;
+
 @ApplicationPath("rest")
 public class RestfulLauncher extends ResourceConfig{
   public RestfulLauncher() {
     packages("com.uf.stock.restful.action");
+    Monitor monitor=SpringBeanFactory.getBean(Monitor.class);
+    monitor.startMonitor();
 }
 }
